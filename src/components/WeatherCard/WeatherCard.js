580863e3eda0e495740727bc499a8261156ba8f8
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-07-29 23:54:06 +0300

    WeatherCard.js was editted.

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -11,23 +11,22 @@ function WeatherCard() {
     <>
       {
         weatherData && (
-        <>
-        <div className="weather-card">
-          <div className="left-bar">
-            <h1>City: {weatherData.name}</h1>
-            <div className="svg-box">
-              <WeatherIcons condition={weatherData.weather[0].main} time={weatherData.weather[0].icon}/>
+          <div className="weather-card">
+            <div className="left-bar">
+              <h1>City: {weatherData.name}</h1>
+              <div className="svg-box">
+                <WeatherIcons condition={weatherData.weather[0].main} time={weatherData.weather[0].icon}/>
+              </div>
+              <p>Temperature: {weatherData.main.temp}</p>
+              <p>Temperature High-Low: {weatherData.main.temp_max} {weatherData.main.temp_min}</p>
             </div>
-            <p>Temperature: {weatherData.main.temp}</p>
-            <p>Temperature High-Low: {weatherData.main.temp_max} {weatherData.main.temp_min}</p>
+            <div className="right-bar">
+              <p>Feels like: {weatherData.main.feels_like}</p>
+              <p>Humidity: {weatherData.main.humidity}</p>
+              <p>Wind: {weatherData.wind.speed}</p>
+            </div>
           </div>
-          <div className="right-bar">
-            <p>Feels like: {weatherData.main.feels_like}</p>
-            <p>Humidity: {weatherData.main.humidity}</p>
-            <p>Wind: {weatherData.wind.speed}</p>
-          </div>
-        </div>
-        </>)
+        )
       }
     </>
   )
